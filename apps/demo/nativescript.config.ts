076cd776882748b4d57cfa91b5e2d50cc8fc505a
Han Xiao <cloud.han.xiao@outlook.com>
2021-10-25 15:23:57 +0200

    update demo

--- apps/demo/nativescript.config.ts
+++ apps/demo/nativescript.config.ts
@@ -1,14 +1,14 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-	id: 'org.nativescript.plugindemo',
-	appResourcesPath: '../../tools/assets/App_Resources',
-	android: {
-		v8Flags: '--expose_gc',
-		markingMode: 'none',
-	},
-	appPath: 'src',
+  id: 'com.nordsense.nativescript.plugindemo.nfc',
+  appResourcesPath: '../../tools/assets/App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none',
+  },
+  appPath: 'src',
   cli: {
-    packageManager: 'npm' 
-  }
+    packageManager: 'npm',
+  },
 } as NativeScriptConfig;
